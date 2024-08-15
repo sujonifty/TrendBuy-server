10c396ad22794b620966edfe5f4e81d943e88de4
sujonifty <sujonifty@gmail.com>
2024-08-16 05:22:32 +0600

    sorting sections completed

--- index.js
+++ index.js
@@ -9,37 +9,58 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS,process.env.DB_USER)
+// console.log(process.env.DB_PASS,process.env.DB_USER)
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const productsCollection = client.db('TrendBuy').collection('products');
+        const productsCollection = client.db('TrendBuy').collection('products');
 
-    app.get('/products', async (req, res) => {
-        const result = await productsCollection.find().toArray();
-        res.send(result);
-      })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page)
+            const size = parseInt(req.query.size)
+            // console.log('current , item', req.query)
+            const result = await productsCollection.find().skip(page * size).limit(size).toArray();
+            res.send(result);
+        })
+
+        app.get('/productsCount', async (req, res) => {
+            const count = await productsCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+
+        app.get('/sort/:sortItem', async (req, res) => {
+            const sortItem=req.params.sortItem;
+            console.log(sortItem)
+            if(sortItem=='price'){
+                const cursor = productsCollection.find().sort({ price: 1 });
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            const cursor = productsCollection.find().sort({ date: 1 });
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
